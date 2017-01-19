621c59ba123df1c808cf6efdd6b703c89a6db296
Jakob Sloth Jepsen <jsj@reseiwe.com>
2017-01-19 09:44:49 +0100

    Use the master off mt76

--- target/linux/ramips/image/mt7688.mk
+++ target/linux/ramips/image/mt7688.mk
@@ -7,7 +7,7 @@ define Device/LinkIt7688
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
   SUPPORTED_DEVICES := linkits7688 linkits7688d
   DEVICE_TITLE := MediaTek LinkIt Smart 7688
-  DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci uboot-envtools
+  DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci uboot-envtools webui
 endef
 TARGET_DEVICES += LinkIt7688
 
